bf2a693c890a582fa8ffd8478668aded8ca481f3
Pieter Loubser <pieter.loubser@puppetlabs.com>
2012-08-08 10:59:29 +0100

    Rewrote hpacucli fact to keep better track of position output string

--- hpacucli.rb
+++ hpacucli.rb
@@ -1,78 +1,61 @@
-require 'pp'
 require 'facter'
 
-def clean_fact(fact)
-  fact.gsub!(" ", "_")
-  fact.downcase!
-  fact.sub(/smart_array_.*?_in_slot_[0-9]+_\(embedded\)/) {|match| return "hparray#{match.scan(/[0-9]+/).last}"}
-  fact.sub(/array:_./) { |match| id = match.slice!(-1); return "array#{id-97}" }
-  fact.sub(/logical_drive:_./) { |match| id = match.slice(-1);return "ld#{id-49}" }
-  fact.sub(/physicaldrive_(\d:\d|\d.:\d:\d)+/) { |match| match = $1;return "pd#{@pd_array.index(match.upcase)}"}
-  fact
-end
-
-def path(h, f_path = [] )
-  cohort = get_siblings(h)
-  cohort.each do |key|
-    if h[key].kind_of?(Hash)
-      h[key].each do |k,v|
-        if v.kind_of?(Hash)
-          f_path.push(key)
-          path(h[key], f_path)
-          f_path.pop
-        elsif v.kind_of?(String)
-          terminal_path = f_path.dup
-          terminal_path << key unless terminal_path.include?(key)
-          terminal_path << k unless terminal_path.include?(k)
-          terminal_path.collect! { |fact| clean_fact(fact.dup)}
-          terminal_path = terminal_path.join('_')
-          Facter.add(terminal_path) do setcode do v end end
-        end
-      end
-    end
-  end
-end
-
-def get_siblings(h)
-  siblings = []
-  h = h.reject{|k,v| !v.kind_of?(Hash)}
-  h.each_key {|k| siblings << k }
-  siblings.sort
-  return siblings
-end
-
 if File.exists?("/usr/sbin/hpacucli")
-
-  p = %x{/usr/sbin/hpacucli controller all show config detail}
-  p_array, p_hash = [], {}
-  @pd_array = []
-  p.each{|line| p_array << [line.chomp.lstrip, line =~ /\S/] unless line.match(/^\s+$/)}
-
-  hash_stack = [p_hash]
-  (0..p_array.length).each do |i|
-    if i < p_array.length-1
-      new_key = p_array[i][0].split(/:/).first
-      new_value = p_array[i][0].split(/:/).last.squeeze(" ").strip
-
-      if p_array[i][0] =~ /physicaldrive .*$/
-        @pd_array << p_array[i][0].split(/\s/).last
-      end
-
-      if p_array[i+1][1] > p_array[i][1]
-        hash_new = {}
-        hash_stack[-1][p_array[i][0]] = hash_new
-        hash_stack.push hash_new
-      elsif p_array[i+1][1] < p_array[i][1]
-        hash_stack[-1][new_key] = new_value
-        hash_stack.pop
-      elsif p_array[i+1][1] == p_array[i][1]
-        hash_stack[-1][new_key] = new_value
-      end
-    elsif i == p_array.length-1
-      hash_stack[-1][new_key] = new_value
-    end
+  raw_data =  %x{/usr/sbin/hpacucli controller all show config detail}
+  raw_data = raw_data.split("\n")
+  raw_data.reject!{|x| x =~ /^\s+$/}
+  raw_data = raw_data.map do |x|
+    x.chomp.strip.squeeze(' ').gsub(' ', '_').gsub(/(\(|\))/, '')
   end
 
-  hash_stack = hash_stack.first
-  path(hash_stack)
+  context_array = []
+  pd = 0
+
+  raw_data.each do |line|
+    popped = ''
+    id = ''
+    case line.downcase
+      when /smart_array_.*?_in_slot_[0-9]+(_\(embedded\))*/
+        context_array.push "hp_array0"
+      when /array:_(.)$/
+        id = $1
+        while(!popped.match(/array:_(.)$/) && context_array.size > 1)
+          popped = context_array.pop
+        end
+        context_array.push "array#{id}"
+      when /logical_drive:_(\d+)/
+        id = $1
+        while(!popped.match(/logical_drive:_(\d+)/) && context_array.size > 2)
+          popped = context_array.pop
+        end
+        pd = 0
+        context_array.push "ld#{id}"
+      when /mirror_group_(\d+)/
+        id = $1
+        while(!popped.match(/mirror_group_(\d+)/) && context_array.size > 3)
+              popped = context_array.pop
+        end
+        context_array.push "mirror_group_#{id}"
+      when /physicaldrive_(\d:\d|\d.:\d:\d)+$/
+        while(!popped.match(/physicaldrive_(\d:\d|\d.:\d:\d)+$/) && context_array.size > 3)
+          popped = context_array.pop
+        end
+        pd += 1 unless popped == ''
+        context_array.push "pd#{pd}"
+      else
+        if context_array.last.downcase.match(/mirror_group_(\d+)$/)
+          key, value = line.split('_', 2)
+          context_array.push key
+          context_array.pop
+        else
+          key, value = line.split(':')
+        end
+        context_array.push key
+        value.gsub!('_', '') if value
+        Facter.add(context_array.join("_").downcase) do setcode do value end end
+        context_array.pop
+      end
+    popped = ''
+    id = ''
+  end
 end
