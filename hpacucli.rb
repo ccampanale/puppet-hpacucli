8febb7aba9a3004934631e9ce9e70b6ea858b389
Pieter Loubser <pieter.loubser@puppetlabs.com>
2012-08-09 11:43:23 +0100

    Stopped trying to add facts with empty fact names

--- hpacucli.rb
+++ hpacucli.rb
@@ -52,7 +52,8 @@ if File.exists?("/usr/sbin/hpacucli")
         end
         context_array.push key
         value.gsub!('_', '') if value
-        (Facter.add(context_array.join("_").downcase) do setcode do value end end) unless context_array.empty?
+        factname = context_array.join("_").downcase
+        (Facter.add(context_array.join("_").downcase) do setcode do value end end) unless factname == ''
         context_array.pop
       end
     popped = ''
